d9a43291cd4ed630389d3ae7d999b3543989d814
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-03-13 13:26:01 -0300

    Update Curso de TypeScript

--- Curso-TypeScript/3-classes/005-gets-and-sets/indes.ts
+++ Curso-TypeScript/3-classes/005-gets-and-sets/indes.ts
@@ -1,7 +1,7 @@
 //Classes
 class Pessoa {
-    nome: string = "";
-    idade: number = 0;
+   protected nome: string = "";
+   protected idade: number = 0;
     //Inicializador
     constructor(nome: string, idade: number) {
         this.nome = nome
@@ -15,3 +15,36 @@ class Pessoa {
         return `O ${this.nome} fez ${++this.idade} anos.`
     }
 }
+
+class Jonathas extends Pessoa {
+    private profissao: string = "programador"
+    constructor(){
+        super("Jonathas", 37)
+    }
+    public getProfissao(){
+      return `${this.nome} trabalha como programador`  
+    }
+ };
+class Ingrid extends Pessoa {
+    private profissao: string = "professora"
+    constructor(){
+        super("Ingrid", 30)
+    }
+     public getProfissao(){
+      return `${this.nome} trabalha como professora`  
+    }
+ };
+
+const jonathas = new Jonathas();
+const ingrid = new Ingrid();
+
+console.log(jonathas.nome);
+console.log(jonathas.idade);
+console.lof(jonathas.comer("Lazanha"));
+console.log(jonathas.fezAniversario());
+console.log(jonathas.getProfissao())
+console.log(ingrid.nome);
+console.log(ingrid.idade);
+console.log(ingrid.comer("Pizza"));
+console.log(ingrid.fezAniversario());
+console.log(ingrid.getProfissao())
